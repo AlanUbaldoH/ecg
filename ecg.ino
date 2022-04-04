99947c7a244006b4f191f1d118ad8fc22a6ed53a
AlanUbaldoH <94560470+AlanUbaldoH@users.noreply.github.com>
2022-04-04 15:00:57 -0500

    Add files via upload

---
+++ ecg.ino
@@ -0,0 +1,19 @@
+
+
+void setup(){
+ // Inicializar la comunicación en serie:
+  Serial.begin(9600);
+  pinMode(10, INPUT); // Configuración para la detección LO +
+  pinMode(11, INPUT); // Configuración para la detección LO -
+}
+void loop() {
+if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+Serial.println('!');
+}
+else{
+// Imprimir la lectura del puerto A0
+Serial.println(analogRead(A0));
+}
+//Espere un poco para evitar que los datos en serie se saturen
+delay(1);
+}
